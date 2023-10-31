d0375100bd35157dee9d6075105a6abaf8cb64c6
RhianHenrique <128731635+RhianHenrique@users.noreply.github.com>
2023-10-31 11:25:19 -0300

    Add files via upload
    Senai EX 06 - Spring

---
+++ senaiEx/src/main/java/com/Senai/senaiEx/SenaiExApplication.java
@@ -0,0 +1,13 @@
+package com.Senai.senaiEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiExApplication.class, args);
+	}
+
+}
---
+++ senaiEx/src/test/java/com/Senai/senaiEx/SenaiExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Senai.senaiEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
